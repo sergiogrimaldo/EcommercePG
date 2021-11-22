15b571fede2cbe0d572aa441595464db2a0a60d0
florlodosa <florencialodosa@gmail.com>
2021-11-22 17:46:06 -0300

    feat(22-11): redux config in store/reducer/actions/app/index-flor

---
+++ client/src/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <h1>App in progress</h1>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
