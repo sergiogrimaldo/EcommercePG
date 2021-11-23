d1452fbc35b384010fde9f8c47b462dd8c8656cb
frann11 <francisco.berthet@gmail.com>
2021-11-22 22:56:48 -0300

    feat/fix(22.11) added function to use staticJson for components, create header fix Card

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import style from './header.style.modules.css'
+
+export default function Header(){
+    return(
+        <header style={{
+            zIndex:0,
+            borderBottom:'1px solid rgba(0,0,0,0.05)',
+            padding:'20px',
+            paddingTop:0,
+            paddingBottom:0,
+            display:'flex',
+            backgroundColor:'white',
+            alignContent:'center',
+            alignItems:'center',
+            justifyContent:'space-between',
+        }}>
+            <h1>ZapAPP</h1>
+            <div>
+                <ul style={{
+                    listStyle:'none',
+                    display:'flex'}}>
+                    <li style={{margin:10}}> Home </li>
+                    <li style={{margin:10}}> Login </li>
+                    <li style={{margin:10}}> Carrito </li>
+                </ul>
+            </div>
+        </header>
+    )
+}
