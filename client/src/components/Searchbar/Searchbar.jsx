cbc2dd1afda5bb04ebecb996108ddc3764841a63
elmaruz <lmarussig@gmail.com>
2021-11-23 13:13:59 +0000

    feat(23.11 /client/src/components/): basic searchbar created - receives function as prop to be invoked upon search button click

---
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function Searchbar({ onSearch }) {
+	let [input, setInput] = useState('');
+
+	function onChangeHandler(e) {
+		setInput(e.target.value);
+	}
+
+	function onSubmitHandler(e) {
+		e.preventDefault();
+		onSearch(input);
+		setInput('');
+	}
+
+	return (
+		<div>
+			Searchbar
+			<div>
+				<form onSubmit={onSubmitHandler}>
+					<input type='text' value={input} onChange={onChangeHandler} />
+					<button type='submit'>Search</button>
+				</form>
+			</div>
+		</div>
+	);
+}
