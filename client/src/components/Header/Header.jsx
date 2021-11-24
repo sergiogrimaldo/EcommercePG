bb4fe4f31f85d2b4d57beea49e299cfa3e5346bc
careyesp95 <careyesp@libertadores.edu.co>
2021-11-24 00:16:56 -0500

    feat(24-11):features-Navbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,61 +1,59 @@
-
+import React from 'react';
 import Searchbar from '../Searchbar/Searchbar.jsx';
 import FilterBrand from '../FilterBrand/FilterBrand.jsx';
-import { useDispatch } from 'react-redux';
-import { openModal } from '../../redux/actions/index.js';
-import { GoogleLogin } from 'react-google-login';
+import Navbar from '../Navbar/Navbar.jsx';
+//import { GoogleLogin } from 'react-google-login';
 
 
 export default function Header({ data }) {
 
-    const responseGoogle = (response) => {
-      console.log(response)
-    }
-      
-	const dispatch = useDispatch()
+    //const responseGoogle = (response) => {
+    //  console.log(response)
+    //}
 
 	return (
-		<header
-			style={{
-				zIndex: 0,
-				borderBottom: '1px solid rgba(0,0,0,0.05)',
-				padding: '20px',
-				paddingTop: 0,
-				paddingBottom: 0,
-				display: 'flex',
-				backgroundColor: 'white',
-				alignContent: 'center',
-				alignItems: 'center',
-				justifyContent: 'space-between',
-			}}>
-			<h1>ZapAPP</h1>
-			<div>
-				<ul
-					style={{
-						cursor: 'pointer',
-						listStyle: 'none',
-						display: 'flex',
-					}}>
-					<Searchbar />
-					<FilterBrand data={data} />
-					<li className='selected' style={{ margin: 10 }}>
-						Home
-					</li>
-					<li style={{ margin: 10 }} onClick={() => dispatch(openModal('signUp'))}> Register </li>
-					<li style={{ margin: 10 }} onClick={() => dispatch(openModal('login'))} > Login </li>
-					<li style={{ margin: 10 }}> Carrito </li>
-           {/* inserto el login con el usuario de google extraido de la documentacion de react-google-login */}
-            <br /> <br />
-            <GoogleLogin 
-            clientId="535679678854-l50v2fpt6e7ag1mhjtc5p1aa1pgv0kcb.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-        />, 
-				</ul>
-			</div>
-		</header>
+		<>
+			<Navbar/>
+			<header
+				style={{
+					zIndex: 0,
+					borderBottom: '1px solid rgba(0,0,0,0.05)',
+					padding: '20px',
+					paddingTop: 0,
+					paddingBottom: 0,
+					display: 'flex',
+					backgroundColor: 'white',
+					alignContent: 'center',
+					alignItems: 'center',
+					justifyContent: 'space-between',
+				}}>
+				<div>
+					<ul
+						style={{
+							cursor: 'pointer',
+							listStyle: 'none',
+							display: 'flex',
+						}}>
+						<Searchbar />
+						<FilterBrand data={data} />
+						{/* <li className='selected' style={{ margin: 10 }}>
+							Home
+						</li>
+						<li style={{ margin: 10 }} onClick={() => dispatch(openModal('signUp'))}> Register </li>
+						<li style={{ margin: 10 }} onClick={() => dispatch(openModal('login'))} > Login </li>
+						<li style={{ margin: 10 }}> Carrito </li> */}
+			{/* inserto el login con el usuario de google extraido de la documentacion de react-google-login */}
+				{/* <GoogleLogin 
+				clientId="535679678854-l50v2fpt6e7ag1mhjtc5p1aa1pgv0kcb.apps.googleusercontent.com"
+				buttonText="Login"
+				onSuccess={responseGoogle}
+				onFailure={responseGoogle}
+				cookiePolicy={'single_host_origin'}
+				/>,  */}
+					</ul>
+				</div>
+			</header>
+		</>
 	);
 }
 
