44cb5ea3fa62bc73da78fbc1d019304c489b4364
Mauricio Alan Castillo <79920621+AlanMauricioCastillo@users.noreply.github.com>
2021-11-24 12:52:11 -0300

    feet:(24/11) finally pude subir el filtro de color

---
+++ client/src/components/FilterColor/FilterColor.jsx
@@ -0,0 +1,54 @@
+import { findGrid } from "./colors";
+import { deleteWord } from "./colors";
+import { splitAll } from "./colors";
+import { deleteAllRepited } from "./colors";
+import { useState } from "react";
+import { filter } from "./colors";
+
+const FilterColor = ({ colors }) => {
+  const [value, setValue] = useState("");
+  console.log(colors);
+
+  let array = splitAll(colors);
+  //console.log(array);
+  let clean = deleteAllRepited(array);
+  let shoes = filter(array, value);
+
+
+
+  shoes.forEach(e => {
+    console.log(e._id);
+  }); // esto es para traer las zapatillas que coinciden con el filtro por id 
+  
+
+
+
+  const onChangeHandler = (e) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <select onChange={onChangeHandler}>
+        <option value="">Filter Color</option>
+        {clean.map(
+          (elem, index) =>
+            !deleteWord.includes(elem) && (
+              <option
+                key={elem + index}
+                value={elem}
+                style={{
+                  backgroundColor: findGrid(elem),
+                }}
+              >
+                {elem[0].toUpperCase() + elem.slice(1)}
+              </option>
+            )
+        )}
+      </select>
+    </div>
+  );
+};
+
+export default FilterColor;
