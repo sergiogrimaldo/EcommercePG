4a5232482c667f5b5904cf91acfb3229c4616e5f
careyesp95 <careyesp@libertadores.edu.co>
2021-11-25 12:00:39 -0500

    feat(25:11):settings-pag

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import Card from "../Card/Card";
+import Paging from '../Paging/Paging.jsx';
 import s from './Cards.module.css';
 
 export default function Cards ({data}) {
+    const [currentPage,setCurrentPage] = useState(1);
+	const [shoes,setShoes] = useState(20);
+	const indexLastShoes = currentPage * shoes;
+	const indexFirstShoes = indexLastShoes - shoes;
+    console.log('Soy data',data)
+	const currentShoes = data?.slice(indexFirstShoes,indexLastShoes);
+
+    console.log('soy data',currentShoes)
+
+	function page(pageNumber){
+		setCurrentPage(pageNumber)
+	}
     return (
-        <div className={s.cards}>
-        { data && data.length > 0 && data.map((shoe) => ( 
-            <Link className={s.links}>
-            <Card 
-            shoeName = {shoe.shoeName} 
-            brand = {shoe.brand}
-            retailPrice = {shoe.retailPrice} 
-            thumbnail = {shoe.thumbnail} 
-            releaseDate = {shoe.releaseDate}    
-            />
-            </Link>
-            )) 
-        }
-        </div>
+        <> 
+            <Paging 
+                allShoes={data?.length}
+                shoesForPage={shoes}
+                page={page}
+			/>
+            <br/>
+            <br/>
+            <div className={s.cards}>
+                { currentShoes && currentShoes.length > 0 && currentShoes.map((shoe) => ( 
+                    <Link className={s.links}>
+                    <Card 
+                    shoeName = {shoe.shoeName} 
+                    brand = {shoe.brand}
+                    retailPrice = {shoe.retailPrice} 
+                    thumbnail = {shoe.thumbnail} 
+                    releaseDate = {shoe.releaseDate}    
+                    />
+                    </Link>
+                    )) 
+                }
+            </div>
+        </>
     )
 }
