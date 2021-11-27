f84714d7d0571dc93d13b7e8197d8d949001c55c
frann11 <francisco.berthet@gmail.com>
2021-11-27 14:28:07 -0300

    feat(27.11): done login and signup logic in backend with JWT

--- api/src/routes/Login.js
+++ api/src/routes/Login.js
@@ -1,23 +1,24 @@
 const { Router } = require("express");
-const express = require('express'),
-      bodyParser = require('body-parser'),
-      jwt = require('jsonwebtoken'),
-      app = express();
+// const { UPSERT } = require("sequelize/types/lib/query-types");
+/// ??????
+const {User} = require('../db')
+// require('dotenv').config();
 
+// const express = require('express'),
+//       bodyParser = require('body-parser'),
+const jwt = require('jsonwebtoken')
 const router = Router();	  
 
-const config = {
-	llave : "miclaveultrasecreta123*"
-}; 
 
-// 1
-app.set('llave', config.llave);
+//console.log(process.env)
+// // 1
+// app.set('llave', config.llave); /// ESTO en .env
 
-// 2
-router.use(bodyParser.urlencoded({ extended: true }));
+// // 2 
+// router.use(bodyParser.urlencoded({ extended: true }));
 
-// 3
-router.use(bodyParser.json());
+// // 3
+// router.use(bodyParser.json());
 
 // 4
 router.get('/', function(req, res) {
@@ -25,12 +26,20 @@ router.get('/', function(req, res) {
 });
 
 
-router.post('/autenticar', (req, res) => {
-    if(req.body.name === "asfo" && req.body.password === "holamundo") {
+router.post('/autenticar', async (req, res) => {
+  const { name, email, password } = req.body
+    // if(req.body.name === "asfo" && req.body.password === "holamundo") {
+    let user = await User.findOne({where:{email:email}} )
+    if ( user && user.length > 0 && user.password === password  ){
+
 		const payload = {
-			check:  true
+      name,
+      email
 		};
-		const token = jwt.sign(payload, router.get(config.llave), {
+
+    /// uso el secreto del .env
+    console.log(process.env.TOKENSECRET)
+		const token = jwt.sign(payload, process.env.TOKENSECRET, {
 			expiresIn: 1440
 		});
 		res.json({
@@ -42,35 +51,36 @@ router.post('/autenticar', (req, res) => {
     }
 })
 
-// 6
-const rutasProtegidas = express.Router(); 
-rutasProtegidas.use((req, res, next) => {
-    const token = req.headers['access-token'];
+// esto todavia no lo mire y por el momento no lo usamos, capaz para las rutas de admin mas adelante
+// // 6
+// const rutasProtegidas = Router(); 
+// rutasProtegidas.use((req, res, next) => {
+//     const token = req.headers['access-token'];
 	
-    if (token) {
-      jwt.verify(token, router.get(config.llave), (err, decoded) => {      
-        if (err) {
-          return res.json({ mensaje: 'Token inválida' });    
-        } else {
-          req.decoded = decoded;    
-          next();
-        }
-      });
-    } else {
-      res.send({ 
-          mensaje: 'Token no proveída.' 
-      });
-    }
- });
+//     if (token) {
+//       jwt.verify(token, router.get(config.llave), (err, decoded) => {      
+//         if (err) {
+//           return res.json({ mensaje: 'Token inválida' });    
+//         } else {
+//           req.decoded = decoded;    
+//           next();
+//         }
+//       });
+//     } else {
+//       res.send({ 
+//           mensaje: 'Token no proveída.' 
+//       });
+//     }
+//  });
 
- router.get('/datos', rutasProtegidas, (req, res) => {
-	const datos = [
-		{ id: 1, name: "Asfo" },
-		{ id: 2, name: "Denisse" },
-		{ id: 3, name: "Carlos" }
-	];
+//  router.get('/datos', rutasProtegidas, (req, res) => {
+// 	const datos = [
+// 		{ id: 1, name: "Asfo" },
+// 		{ id: 2, name: "Denisse" },
+// 		{ id: 3, name: "Carlos" }
+// 	];
 	
-	res.json(datos);
-});
+// 	res.json(datos);
+// });
 
 module.exports= router
