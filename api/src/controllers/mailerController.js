b82732781507c055e68f8834136407551da6c39b
frann11 <francisco.berthet@gmail.com>
2021-11-27 21:03:43 -0300

    feat(27.11): mail service, controllers and route, fix login

---
+++ api/src/controllers/mailerController.js
@@ -0,0 +1,14 @@
+const {sendMail} = require('../services/mailerService')
+
+const sendOrderDetails = async function ( req,res){
+    const {name, email, adress} = req.body
+    try {
+        await sendMail({payload:{name,email,adress}})
+        res.json('mail mandado con exito')
+    }
+    catch (error){
+        res.status(404).json(error)
+    }
+}
+
+module.exports = {sendOrderDetails}
