17c4b76e471a64adc34bd2c870f44f745883b21c
elmaruz <lmarussig@gmail.com>
2021-12-01 17:00:52 +0000

    fix(01.12): Validation added in reducers and across all components which consume api data

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -3,227 +3,229 @@
 // import {
 
 // } from "../constants";
-import isEmpty from "lodash/isEmpty";
+import isEmpty from 'lodash/isEmpty';
 
 const initialState = {
-    shoes: [],
-    filteredShoes: [],
-    modalBuyDetails: {},
-    brands: [],
-    sizes: [],
-    prices: [],
-    modal: "",
-    filters: [],
-    currentPage: 0,
-    filterBrands: [],
-    filterSizes: [],
-    filterPrice: 0,
-    textToSearch: "",
-    cart: [],
-    user: {},
-    isAuthenticaded: false,
+	shoes: [],
+	filteredShoes: [],
+	modalBuyDetails: {},
+	brands: [],
+	sizes: [],
+	prices: [],
+	modal: '',
+	filters: [],
+	currentPage: 0,
+	filterBrands: [],
+	filterSizes: [],
+	filterPrice: 0,
+	textToSearch: '',
+	cart: [],
+	user: {},
+	isAuthenticaded: false,
 };
 
 function rootReducer(state = initialState, action) {
-    switch (action.type) {
-        case "SEND_ORDER_DETAILS":
-            return {
-                ...state,
-            };
+	switch (action.type) {
+		case 'SEND_ORDER_DETAILS':
+			return {
+				...state,
+			};
 
-        case "SET_CURRENT_USER":
-            return {
-                ...state,
-                isAuthenticaded: !isEmpty(action.user),
-                user: action.user,
-            };
+		case 'SET_CURRENT_USER':
+			return {
+				...state,
+				isAuthenticaded: !isEmpty(action.user),
+				user: action.user,
+			};
 
-        case "DELETE_FROM_CART":
-            return {
-                ...state,
-                cart: state.cart.filter((item) => item.name != action.payload),
-            };
+		case 'DELETE_FROM_CART':
+			return {
+				...state,
+				cart: state.cart.filter(item => item.name != action.payload),
+			};
 
-        case "SEARCH":
-            var aux = state.filteredShoes;
-            if (action.payload.length > 0) {
-                return {
-                    ...state,
-                    shoes: aux.filter((elem) => elem.shoeName.toLowerCase().includes(action.payload.toLowerCase())),
-                };
-            } else {
-                return {
-                    ...state,
-                    shoes: aux,
-                };
-            }
+		case 'SEARCH':
+			var aux = state.filteredShoes;
+			if (action.payload.length > 0) {
+				return {
+					...state,
+					shoes: aux.filter(elem =>
+						elem.shoeName.toLowerCase().includes(action.payload.toLowerCase())
+					),
+				};
+			} else {
+				return {
+					...state,
+					shoes: aux,
+				};
+			}
 
-        case "LOGIN":
-            return {
-                ...state,
-                user: action.payload,
-            };
+		case 'LOGIN':
+			return {
+				...state,
+				user: action.payload,
+			};
 
-        case "OPEN_BUY_DETAILS_MODAL":
-            return {
-                ...state,
-                modalBuyDetails: action.payload,
-            };
+		case 'OPEN_BUY_DETAILS_MODAL':
+			return {
+				...state,
+				modalBuyDetails: action.payload,
+			};
 
-        case "LOGOUT":
-            return {
-                ...state,
-                user: {},
-            };
-        case "ADD_TO_CART":
-            state.cart &&
-                state.cart.map((item) => {
-                    if (item.name == action.payload.name) {
-                        return (item.cuantity = item.cuantity + 1 || 1);
-                    } else {
-                        return (item.cuantity = item.cuantity);
-                    }
-                });
+		case 'LOGOUT':
+			return {
+				...state,
+				user: {},
+			};
+		case 'ADD_TO_CART':
+			state.cart &&
+				state.cart.map(item => {
+					if (item.name == action.payload.name) {
+						return (item.cuantity = item.cuantity + 1 || 1);
+					} else {
+						return (item.cuantity = item.cuantity);
+					}
+				});
 
-            state.cart.push({
-                image: action.payload.image,
-                name: action.payload.name,
-                cuantity: action.payload.cuantity,
-                price: action.payload.price,
-                subtotal: action.payload.price * action.payload.cuantity,
-            });
-            return {
-                ...state,
-            };
+			state.cart.push({
+				image: action.payload.image,
+				name: action.payload.name,
+				cuantity: action.payload.cuantity,
+				price: action.payload.price,
+				subtotal: action.payload.price * action.payload.cuantity,
+			});
+			return {
+				...state,
+			};
 
-        case "REMOVE_FROM_CART":
-            state.cart.map((item) => {
-                if (item.name == action.payload.name) {
-                    return (item.cuantity = item.cuantity - 1);
-                } else {
-                    return (item.cuantity = item.cuantity);
-                }
-            });
+		case 'REMOVE_FROM_CART':
+			state.cart.map(item => {
+				if (item.name == action.payload.name) {
+					return (item.cuantity = item.cuantity - 1);
+				} else {
+					return (item.cuantity = item.cuantity);
+				}
+			});
 
-            return {
-                ...state,
-                cart: state.cart.filter((item) => item.cuantity > 0),
-            };
+			return {
+				...state,
+				cart: state.cart.filter(item => item.cuantity > 0),
+			};
 
-        case "GET_SHOES":
-            let array = state.shoes.map(e => e._id);
-            let notRepited = action.payload.filter(e => !array.includes(e._id));
-            return {
-                ...state,
-                shoes: notRepited,
-                filteredShoes: action.payload,
-                brands: action.payload.map((elem) => elem.brand.name),
-                /* sizes: [...new Set(action.payload
+		case 'GET_SHOES':
+			let array = state.shoes && state.shoes.map(e => e._id);
+			let notRepited = action.payload.filter(e => !array?.includes(e._id));
+			return {
+				...state,
+				shoes: notRepited,
+				filteredShoes: action.payload,
+				brands: action.payload.map(elem => elem.brand.name),
+				/* sizes: [...new Set(action.payload
                     .map((elem) => elem.resellPrices) // mapping data's resellPrices properties
                     .filter((elem) => elem) // filtering undefined ones out
                     .map((elem) => Object.keys(elem.flightClub)) // taking all sizes
                     .flat(Infinity)
                     ), 
                 ].sort((a, b) => a - b),*/
-                filters: [],
-                currentPage: 0,
-                cart: state.cart || [],
-                isAuthenticaded: state.isAuthenticaded || true,
-                user: state.user || {},
-            }; // flattening out the array
+				filters: [],
+				currentPage: 0,
+				cart: state.cart || [],
+				isAuthenticaded: state.isAuthenticaded || true,
+				user: state.user || {},
+			}; // flattening out the array
 
-        case "OPEN_MODAL":
-            return {
-                ...state,
-                modal: action.payload,
-            };
+		case 'OPEN_MODAL':
+			return {
+				...state,
+				modal: action.payload,
+			};
 
-            case "GET_PRICES":
-            return {
-                ...state,
-                prices: action.payload,
-            };
+		case 'GET_PRICES':
+			return {
+				...state,
+				prices: action.payload,
+			};
 
-            case "GET_AVAILABLE_SIZES":
-            return {
-                ...state,
-                sizes: action.payload,
-            };
+		case 'GET_AVAILABLE_SIZES':
+			return {
+				...state,
+				sizes: action.payload,
+			};
 
-        case "CLOSE_MODAL":
-            return {
-                ...state,
-                modal: "",
-            };
-        case "SET_FILTER_BRANDS": {
-            if (action.payload) {
-                return {
-                    ...state,
-                    filters: Array.from(new Set([...state.filters, "brands"])),
-                    filterBrands: action.payload,
-                    currentPage: 0,
-                };
-            } else {
-                return {
-                    ...state,
-                    filters: state.filters.filter((elem) => elem !== "brands"),
-                    filterBrands: [],
-                    currentPage: 0,
-                };
-            }
-            break;
-        }
-        case "FILTER_SIZE": {
-            var auxBrands = state.filteredShoes;
-            if (action.payload > 0) {
-                return {
-                    ...state,
-                    filters: Array.from(new Set([...state.filters, "sizes"])),
-                    filterSizes: action.payload,
-                    currentPage: 0,
-                };
-            } else {
-                return {
-                    ...state,
-                    filters: state.filters.filter((elem) => elem !== "sizes"),
-                    filterSizes: [],
-                    currentPage: 0,
-                };
-            }
-        }
-        case "FILTER_PRICE":
-            if (action.payload) {
-                return {
-                    ...state,
-                    filters: Array.from(new Set([...state.filters, "price"])),
-                    filterPrice: action.payload,
-                    currentPage: 0,
-                };
-            } else {
-                return {
-                    ...state,
-                    filters: state.filters.filter((elem) => elem !== "price"),
-                    filterPrice: [],
-                    currentPage: 0,
-                };
-            }
+		case 'CLOSE_MODAL':
+			return {
+				...state,
+				modal: '',
+			};
+		case 'SET_FILTER_BRANDS': {
+			if (action.payload) {
+				return {
+					...state,
+					filters: Array.from(new Set([...state.filters, 'brands'])),
+					filterBrands: action.payload,
+					currentPage: 0,
+				};
+			} else {
+				return {
+					...state,
+					filters: state.filters.filter(elem => elem !== 'brands'),
+					filterBrands: [],
+					currentPage: 0,
+				};
+			}
+			break;
+		}
+		case 'FILTER_SIZE': {
+			var auxBrands = state.filteredShoes;
+			if (action.payload > 0) {
+				return {
+					...state,
+					filters: Array.from(new Set([...state.filters, 'sizes'])),
+					filterSizes: action.payload,
+					currentPage: 0,
+				};
+			} else {
+				return {
+					...state,
+					filters: state.filters.filter(elem => elem !== 'sizes'),
+					filterSizes: [],
+					currentPage: 0,
+				};
+			}
+		}
+		case 'FILTER_PRICE':
+			if (action.payload) {
+				return {
+					...state,
+					filters: Array.from(new Set([...state.filters, 'price'])),
+					filterPrice: action.payload,
+					currentPage: 0,
+				};
+			} else {
+				return {
+					...state,
+					filters: state.filters.filter(elem => elem !== 'price'),
+					filterPrice: [],
+					currentPage: 0,
+				};
+			}
 
-        case "UPDATE": {
-            return {
-                ...state,
-                cart: [...state.cart],
-            };
-        }
-        case "SET_PAGE": {
-            return {
-                ...state,
-                currentPage: action.payload,
-            };
-        }
+		case 'UPDATE': {
+			return {
+				...state,
+				cart: [...state.cart],
+			};
+		}
+		case 'SET_PAGE': {
+			return {
+				...state,
+				currentPage: action.payload,
+			};
+		}
 
-        default:
-            return state;
-    }
+		default:
+			return state;
+	}
 }
 
 export default rootReducer;
