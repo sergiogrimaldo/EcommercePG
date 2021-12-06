6ca09bc97c110273fbb4eb9b7909256be352d9f1
Mauricio Alan Castillo <79920621+AlanMauricioCastillo@users.noreply.github.com>
2021-12-06 00:04:23 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -17,13 +17,13 @@ server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-/* server.use((req, res, next) => {
+server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-}); */
+});
 
 
 server.use('/', routes);
