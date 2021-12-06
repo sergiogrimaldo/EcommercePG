8ca1fabaab6aa0fe3d26ec4d50cd1a51c2bbd1d5
elmaruz <lmarussig@gmail.com>
2021-12-06 10:13:00 +0000

    feat(06.12) Basic css added

--- client/src/components/DeleteShoe/DeleteShoe.jsx
+++ client/src/components/DeleteShoe/DeleteShoe.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { deleteShoe } from '../../redux/actions/index.js';
+import styles from './DeleteShoe.module.css';
 
 export default function DeleteShoe({ id }) {
 	const history = useHistory();
@@ -14,7 +15,9 @@ export default function DeleteShoe({ id }) {
 
 	return (
 		<div>
-			<button onClick={onClickHandler}>X</button>
+			<button className={`${styles.btn_delete}`} onClick={onClickHandler}>
+				X
+			</button>
 		</div>
 	);
 }
