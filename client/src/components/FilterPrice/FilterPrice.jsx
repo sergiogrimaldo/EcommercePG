b90040fb3aa6289cb29df8a3ff0cbb64350ba4f8
elmaruz <lmarussig@gmail.com>
2021-12-08 14:05:16 +0000

    fix(08.12) Default FilterPrice set back to max price

--- client/src/components/FilterPrice/FilterPrice.jsx
+++ client/src/components/FilterPrice/FilterPrice.jsx
@@ -1,33 +1,33 @@
-import { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { filterPrice } from "../../redux/actions/index.js";
-import styles from "./FilterPrice.module.css";
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { filterPrice } from '../../redux/actions/index.js';
+import styles from './FilterPrice.module.css';
 
-export default function FilterPrice({data}) {
-    const dispatch = useDispatch();
-    let [value, setValue] = useState(0);
-    //let data = useSelector((state) => state.prices);
-    if (data) {
-        data = data.map(item => item.retailPrice).filter(item => item);
-        let maxPrice = Math.max(...data);
-        var roundUpMax = (Math.trunc(maxPrice / 100) + 1) * 100;
-    }
+export default function FilterPrice({ data }) {
+	const dispatch = useDispatch();
+	let [value, setValue] = useState(0);
+	//let data = useSelector((state) => state.prices);
+	if (data) {
+		data = data.map(item => item.retailPrice).filter(item => item);
+		let maxPrice = Math.max(...data);
+		var roundUpMax = (Math.trunc(maxPrice / 100) + 1) * 100;
+	}
 
-    useEffect(() => {
-        if (!value || value === -Infinity) {
-            //setValue(roundUpMax);
-        }
-    });
+	useEffect(() => {
+		if (!value || value === -Infinity) {
+			setValue(roundUpMax);
+		}
+	});
 
-    function onChangeHandler(e) {
-        setValue(e.target.value);
-        dispatch(filterPrice(e.target.value));
-    }
+	function onChangeHandler(e) {
+		setValue(e.target.value);
+		dispatch(filterPrice(e.target.value));
+	}
 
-    return (
-        <div>
-            <input type="range" min="0" max={roundUpMax} step="50" value={value} onChange={onChangeHandler} />
-            <label className={`${value !== -Infinity ? styles.label : styles.labelHide}`}>{`$${value}`}</label>
-        </div>
-    );
+	return (
+		<div>
+			<input type='range' min='0' max={roundUpMax} step='50' value={value} onChange={onChangeHandler} />
+			<label className={`${value !== -Infinity ? styles.label : styles.labelHide}`}>{`$${value}`}</label>
+		</div>
+	);
 }
