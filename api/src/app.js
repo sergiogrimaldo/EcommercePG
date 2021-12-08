aca82b6a9d051d6e8a94167e497909190a89e493
Sergio Grimaldo <47435264+sergiogrimaldo@users.noreply.github.com>
2021-12-07 22:21:05 -0300

    Update app.js

--- api/src/app.js
+++ api/src/app.js
@@ -5,7 +5,6 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require('cors');
 
-let origin = process.env.PORT === '3000' ? 'http://localhost:3000' : 'https://ecommerce-pg.vercel.app';
 
 require('./db.js');
 
@@ -20,7 +19,7 @@ server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', origin); // update to match the domain you will make the request from
+	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
