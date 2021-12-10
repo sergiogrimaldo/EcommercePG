a5e779f5bd520a10b390f035407482a84ecdb54e
Mauricio Alan Castillo <79920621+AlanMauricioCastillo@users.noreply.github.com>
2021-12-10 12:49:21 -0300

    trie 2

--- api/src/app.js
+++ api/src/app.js
@@ -22,7 +22,6 @@ server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
 	next();
 });
 
