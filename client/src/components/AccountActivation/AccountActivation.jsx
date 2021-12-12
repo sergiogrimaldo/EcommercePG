45a6b3a4bf99c452b20a8d3c012bea2b6a9b9ddf
florlodosa <florencialodosa@gmail.com>
2021-12-11 23:24:56 -0300

    feat(11-12)Validate email now it´s working

--- client/src/components/AccountActivation/AccountActivation.jsx
+++ client/src/components/AccountActivation/AccountActivation.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import axios from "axios";
 
+export default function AccountActivation({token}) {
 
-export default function AccountActivation() {
-
+    useEffect(async()=> 
+    await axios.post(`http://localhost:3001/users/activateAccount/${token}`),
+    [])
     return (
         <div style={{display: 'grid', justifyContent: 'center', alignItems:'center', padding:220, textAlign: 'center'}}>
             <h2>Your account has been activated!</h2>
