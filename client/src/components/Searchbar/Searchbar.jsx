8fff8bb545e6f7f963350e6ff6ffe19d7fcd1380
Sergio Grimaldo <47435264+sergiogrimaldo@users.noreply.github.com>
2021-12-13 15:19:57 -0500

    header responsive

--- client/src/components/Searchbar/Searchbar.jsx
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { search } from '../../redux/actions';
+import './search.css'
 export default function Searchbar() {
 	let [input, setInput] = useState('');
 	let dispatch = useDispatch();
@@ -15,10 +16,10 @@ export default function Searchbar() {
 	}
 
 	return (
-		<div>
-			<form style={{display:'flex', width: '150%'}} onSubmit={onSubmitHandler}>
-				<input style={{borderTopLeftRadius:15, borderBottomLeftRadius:15, width:'100%', padding:5}} type='text' value={input} onChange={onChangeHandler} />
-				<button type='submit'>Search</button>
+		<div className='search'>
+			<form style={{display:'flex', width: '100%'}} onSubmit={onSubmitHandler}>
+				<input style={{borderTopLeftRadius:15, borderBottomLeftRadius:15,width:'90%',padding:5}} type='text' value={input} onChange={onChangeHandler} />
+				<button style={{borderTopRightRadius:15, borderBottomRightRadius:15}} type='submit'>Search</button>
 			</form>
 		</div>
 	);
