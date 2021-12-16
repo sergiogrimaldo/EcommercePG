0357e073143b6b255d4f5080968144fb5d2c2ecf
careyesp95 <careyesp@libertadores.edu.co>
2021-12-16 02:25:09 -0500

    feat(16-12):pagging-version2

--- client/src/components/Paging/Paging.jsx
+++ client/src/components/Paging/Paging.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setPage } from "../../redux/actions";
 import style from "./Paging.module.css";
@@ -7,48 +7,78 @@ function Paging({ shoes, shoesPerPage }) {
     shoes = shoes || [];
 
     const page = useSelector((state) => state.currentPage);
+    const [pageNumberLimit, setPageNumberLimit] = useState(5);
+    const [maxPageLimit, setMaxPageLimit] = useState(5);
+    const [minPageLimit, setMinPageLimit] = useState(1);
+
 
     const dispach = useDispatch();
 
-    const pageNumber = ["<<"];
+    //const pageNumber = ["<<"];
+    let pageNumber = [];
 
+    
+    
     for (let i = 1; i <= Math.ceil(shoes.length / shoesPerPage); i++) {
         pageNumber.push(i);
     }
-    pageNumber.push([">>"]);
+    //pageNumber.push([">>"]);
+    let pageN = pageNumber.pop();
 
-    const handleButton = function changePage(e) {
-        //console.log(e);
-        if (e == "<<") {
-            if (page > 0) {
-                dispach(setPage(page - 1));
-            }
-        } else if (e == ">>") {
-            if (page < Math.ceil(shoes.length / shoesPerPage) - 1) {
-                dispach(setPage(page + 1));
-            }
-        } else {
-            dispach(setPage(e - 1));
+
+    const handleClick = function(e){
+        dispach(setPage(e));
+    }
+
+    const handleButtonNext = function(){
+        dispach(setPage(page + 1));
+        if(page + 1 > maxPageLimit){
+            setMaxPageLimit(maxPageLimit + pageNumberLimit);
+            setMinPageLimit(minPageLimit + pageNumberLimit);
         }
-    };
-//console.log(window.screen.width)
-return (
-    <nav className={style.nav}>
-        <div className={style.containerPage}>
-            {pageNumber &&
-                pageNumber.map((e) => {
-                    return (
-                        <div key={e}>
-                            <button key={"button" + e} className={style.containerBTN} onClick={() => handleButton(e)}>
-                                {e}
-                            </button>
-                        </div>
-                    );
-                })}
-        </div>
-    </nav>
-);
-}
 
+    }
+
+    const handleButtonPrev = function (){
+        dispach(setPage(page - 1));
+        if((page - 1) % pageNumberLimit == 0){
+            setMaxPageLimit(maxPageLimit - pageNumberLimit);
+            setMinPageLimit(minPageLimit - pageNumberLimit);
+        }
+    }
+//console.log(window.screen.width)
+    const renderPage = pageNumber &&
+    pageNumber.map((e) => {
+        if(e < maxPageLimit + 1 && e > minPageLimit - 1){
+            return (
+                <div key={e}>
+                    <button key={"button" + e} className={page == e ? style.active:style.containerBTN} onClick={() => handleClick(e)}>
+                        { e }
+                    </button>
+                </div>
+            );
+
+        }
+    })
+
+
+    return (
+        <nav className={style.nav}>
+            <div className={style.containerPage}>
+                <button
+                className={style.containerBTN}
+                onClick={handleButtonPrev}
+                disabled={page == pageNumber[0]?true:false}
+                >Prev</button>
+                {renderPage}
+                <button
+                className={style.containerBTN}
+                onClick={handleButtonNext}
+                disabled={page == pageNumber[pageNumber.length - 1]?true:false}
+                >Next</button>           
+            </div>
+        </nav>
+    );
+}
 
 export default Paging;
